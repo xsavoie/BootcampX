796ecb448df0f130a700bbc1095b1131607278bf
Xavier Savoie <xaviersavoie@gmail.com>
2021-10-12 17:11:52 +0000

    Created teachers and assistance_requests table and populated with fake data

--- 0_selects/1_students_without_github.sql
+++ 0_selects/1_students_without_github.sql
@@ -2,3 +2,11 @@ SELECT id, name, email, cohort_id
 FROM students 
 WHERE github IS NULL
 ORDER BY cohort_id;
+
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING total_submissions < 100;
